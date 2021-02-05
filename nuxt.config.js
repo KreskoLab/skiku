4927c688b76130e4ddb48d79fce2b86edbf47ea1
KreskoLab <redeezol@gmail.com>
2021-02-05 19:49:49 +0200

    update assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,13 +14,13 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    '@mdi/font/css/materialdesignicons.css',
-    '~/assets/main.scss'
+    '@/assets/materialdesignicons.css',
+    '@/assets/main.scss'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    {src: '~/plugins/buefy.js'}
+    {src: '@/plugins/buefy.js'}
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
