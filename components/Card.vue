c544d2b99e99052e5b95798fdef4865b5d6d807b
KreskoLab <redeezol@gmail.com>
2021-02-09 19:30:31 +0200

    bugs fix

--- components/Card.vue
+++ components/Card.vue
@@ -3,56 +3,51 @@
   <div class="card">
 
     <div class="card-image">
-      <figure class="image is-1by1" style="padding-top:unset">
+      <figure class="image is-1by1 pt-0">
         <nuxt-picture placeholder :src="good.img.s350x350" v-if="store.name !== 'Сільпо' " />
         <nuxt-picture placeholder :src="good.mainImage" v-else />
       </figure>
     </div>
 
     <div style="margin-left:-10px" class="card-content">
+
       <div class="media">
-
-        <div class="media-left" v-if="store.name == 'Сільпо' ">
-
-          <b-taglist attached v-if="good.priceStopAfter !== null">
-              <b-tag type="is-danger is-light">
-                <b-icon icon="sale"></b-icon>
-              </b-tag>
-              <b-tag style="margin-left:-5px" type="is-danger is-light"><span class="is-size-6">{{good.price}} грн <span class="is-size-7">до {{ good.priceStopAfter.toString().substr(0, 5) }} </span> </span></b-tag>
-          </b-taglist>
-
-          <b-taglist attached v-else>
-              <b-tag type="is-success is-light">
-                <b-icon icon="cash"></b-icon>
-              </b-tag>
-              <b-tag style="margin-left:-5px" type="is-success is-light"><span class="is-size-6">{{good.price}} грн</span></b-tag>
-          </b-taglist>
-
+        <div class="media-content has-text-centered">
+          <p id="name" class="has-text-weight-medium" v-if="store.name == 'Сільпо' ">{{good.name}}</p>
+          <p id="name" class="has-text-weight-medium" v-else>{{good.title}}</p>
         </div>
-
-        <div class="media-left" v-else>
-
-          <b-taglist attached v-if="good.discount.status == true">
-              <b-tag type="is-danger is-light">
-                <b-icon icon="sale"></b-icon>
-              </b-tag>
-              <b-tag style="margin-left:-5px" type="is-danger is-light"><span class="is-size-6">{{ good.price.toString().substring(0, good.price.toString().length - 2) + '.' + good.price.toString().slice(-2) }} грн <span class="is-size-7">до {{ good.discount.due_date.toString().substr(8, 4) +'.'+ good.discount.due_date.substr(5, 2) }} </span> </span></b-tag>
-          </b-taglist>
-
-          <b-taglist attached v-else>
-              <b-tag type="is-success is-light">
-                <b-icon icon="cash"></b-icon>
-              </b-tag>
-              <b-tag style="margin-left:-5px" type="is-success is-light"><span class="is-size-6">{{ good.price.toString().substring(0, good.price.toString().length - 2) + '.' + good.price.toString().slice(-2) }} грн</span></b-tag>
-          </b-taglist>
-
-        </div>
-
       </div>
 
-      <div class="content" style="margin-top:-10px">
-        <span class="has-text-weight-medium" v-if="store.name == 'Сільпо' ">{{good.name}}</span>
-        <span class="has-text-weight-medium" v-else>{{good.title}}</span>
+      <div class="content has-text-centered">
+
+        <div v-if="store.name == 'Сільпо' ">
+
+          <b-tag v-if="good.priceStopAfter" type="is-danger is-light">
+            <span class="is-size-5">
+              <грн>{{good.price}}<span class="is-size-6"> до {{ good.priceStopAfter.toString().substr(0, 5) }} </span> 
+            </span>
+          </b-tag>
+      
+          <b-tag v-else type="is-success is-light">
+            <span class="is-size-5"><грн> {{good.price}}</span>
+          </b-tag>
+        
+        </div>
+
+        <div v-else>
+
+          <b-tag v-if="good.discount.status" type="is-danger is-light">
+            <span class="is-size-5"><грн> {{ good.price.toString().substring(0, good.price.toString().length - 2) + '.' + good.price.toString().slice(-2) }}
+              <span class="is-size-6"> до {{ good.discount.due_date.toString().substr(8, 4) +'.'+ good.discount.due_date.substr(5, 2) }} </span>
+            </span>
+          </b-tag>
+
+          <b-tag v-else type="is-success is-light">
+            <span class="is-size-5"><грн> {{ good.price.toString().substring(0, good.price.toString().length - 2) + '.' + good.price.toString().slice(-2) }}</span>
+          </b-tag>
+
+        </div>
+
       </div>
 
     </div>
@@ -67,3 +62,9 @@ export default {
 }
 
 </script>
+
+<style>
+#name{
+  
+}
+</style>
--- components/CardMobile.vue
+++ components/CardMobile.vue
@@ -3,36 +3,55 @@
   <div class="card">
 
     <div class="card-image has-text-centered">
-      <figure class="image is-128x128 is-inline-block" style="padding-top:unset">
+      <figure class="image is-128x128 is-inline-block">
         <nuxt-picture placeholder :src="good.img.s350x350" v-if="store.name !== 'Сільпо' " />
         <nuxt-picture placeholder :src="good.mainImage" v-else />
       </figure>
     </div>
 
     <div class="card-content">
+
       <div class="media">
-         
-        <div class="media-left" v-if="store.name == 'Сільпо' ">
-
-          <b-tag v-if="good.promotion == null" style="margin-left:-5px" type="is-success is-light"><span class="is-size-6">{{good.price}} грн</span></b-tag>
-
-          <b-tag v-else style="margin-left:-5px" type="is-danger is-light"><span class="is-size-6">{{good.price}} грн <span class="is-size-7">до {{ good.priceStopAfter.toString().substr(0, 5) }} </span> </span></b-tag>
-
+        <div class="media-content has-text-centered">
+          <p id="name" class="has-text-weight-medium" v-if="store.name == 'Сільпо' ">{{good.name}}</p>
+          <p id="name" class="has-text-weight-medium" v-else>{{good.title}}</p>
         </div>
-
-        <div class="media-left" v-else>
-
-          <b-tag v-if="good.discount.status == true" style="margin-left:-5px" type="is-danger is-light"><span class="is-size-6">{{ good.price.toString().substring(0, good.price.toString().length - 2) + '.' + good.price.toString().slice(-2) }} грн <span class="is-size-7">до {{ good.discount.due_date.toString().substr(8, 4) +'.'+ good.discount.due_date.substr(5, 2) }} </span> </span></b-tag>
-
-          <b-tag v-else style="margin-left:-5px" type="is-success is-light"><span class="is-size-6">{{ good.price.toString().substring(0, good.price.toString().length - 2) + '.' + good.price.toString().slice(-2) }} грн</span></b-tag>
-
-        </div>
-
       </div>
 
-      <div class="content" style="margin-top:-10px">
-        <span class="has-text-weight-medium" v-if="store.name == 'Сільпо' ">{{good.name}}</span>
-        <span class="has-text-weight-medium" v-else>{{good.title}}</span>
+      <div class="content has-text-centered" style="margin-top:-10px">
+
+        <div v-if="store.name == 'Сільпо' ">
+
+          <b-tag v-if="!good.promotion" type="is-success is-light">
+            <span class="is-size-5"><грн> {{good.price}}</span>
+          </b-tag>
+
+          <b-tag v-else type="is-danger is-light">
+            <span class="is-size-5">
+              <грн> {{good.price}} 
+              <span class="is-size-6"> до {{ good.priceStopAfter.toString().substr(0, 5) }} </span> 
+            </span>
+          </b-tag>
+
+        </div>
+
+        <div v-else>
+
+          <b-tag v-if="good.discount.status" type="is-danger is-light">
+            <span class="is-size-5">
+              <грн> {{ good.price.toString().substring(0, good.price.toString().length - 2) + '.' + good.price.toString().slice(-2) }}
+              <span class="is-size-6"> до {{ good.discount.due_date.toString().substr(8, 4) +'.'+ good.discount.due_date.substr(5, 2) }} </span> 
+            </span>
+          </b-tag>
+
+          <b-tag v-else type="is-success is-light">
+            <span class="is-size-5">
+              <грн> {{ good.price.toString().substring(0, good.price.toString().length - 2) + '.' + good.price.toString().slice(-2) }}
+            </span>
+          </b-tag>
+
+        </div>
+
       </div>
 
     </div>
