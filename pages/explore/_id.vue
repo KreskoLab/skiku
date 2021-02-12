a2ac3eb1a6e5796672c4f37b44cc76739c86ee74
KreskoLab <redeezol@gmail.com>
2021-02-12 16:04:58 +0200

    Add files via upload

--- pages/explore/_id.vue
+++ pages/explore/_id.vue
@@ -1,12 +1,11 @@
 <template>
-
-<div>
+  <div>
    
     <Navbar :cities="cities" :city="city" />
 
-    <div v-if="!$fetchState.pending" class="mobile is-hidden-tablet px-2">
+    <div v-if="!$fetchState.pending" class="is-hidden-desktop px-2">
         
-        <nav class="level is-mobile" style="margin-top:25px">
+        <nav class="level is-mobile mt-5 mx-2">
 
           <div class="level-left">
             <p class="has-text-weight-medium">{{count}} товарів</p>
@@ -21,85 +20,82 @@
         <div class="columns is-multiline">
 
           <div class="column px-4" v-for="(good,index) in goods" :key="good.ean">
-            <CardMobile :good="good" :store="storeSelect" />
+            <Card :good="good" :store="storeSelect" />
           </div>
 
         </div>
 
-        <PaginationMobile :count="count" :page="page" />
+        <Pagination :count="count" :page="page" />
 
     </div>
 
-    <div class="desktop is-hidden-mobile" style="padding-left:250px">
+    <div class="columns is-hidden-touch">
 
-    <div class="sidebar">
+      <div class="column is-2">
 
-        <div style="padding: 15px 16px 16px 16px" id="Stores"> 
+          <div class="mt-5 px-4"> 
 
-           <p class="is-size-5 has-text-success has-text-weight-medium">Магазин</p>
+            <p class="is-size-5 has-text-success has-text-weight-medium">Магазин</p>
 
-            <div class="field" style="display:table-caption;margin-left:5px;margin-top:8px">
+              <div class="mt-2 ml-1">
 
-              <b-radio v-if="store[city.data]" v-model="storeSelect" :native-value="store" type="is-success" style="padding-bottom:10px" v-for="(store,index) in stores" :key="store.name">
-                  <figure style="margin-left:10px;margin-right:5px" class="image is-16x16">
-                    <img :src="store.img">
-                  </figure>
-                  <span class="has-text-weight-medium" style="white-space:pre">{{store.name}}</span>
-              </b-radio>
+                <b-radio v-if="store[city.data]" v-model="storeSelect" :native-value="store" class="mb-2" type="is-success" v-for="(store,i) in stores" :key="store.i">
+                    <figure class="image is-16x16 mx-2">
+                      <img :src="store.img">
+                    </figure>
+                    <span class="has-text-weight-medium" style="white-space:pre">{{store.name}}</span>
+                </b-radio>
 
-            </div>
+              </div>
 
-        </div>
+          </div>
 
-        <div style="padding: 0 16px 16px 16px;margin-top:-10px">
+          <div class="px-4">
 
-           <p class="is-size-5 has-text-success has-text-weight-medium">Товари</p>
+            <p class="is-size-5 has-text-success has-text-weight-medium">Товари</p>
 
-            <div style="margin-top:8px;margin-left:5px;display:table-caption">
+              <div class="mt-2 ml-1">
 
-              <b-radio v-model="categorySelect" :native-value="category" type="is-success" style="display:flex;padding-bottom:10px" v-for="(category,index) in categories" :key="category.name">
+                <b-radio v-model="categorySelect" :native-value="category" class="mb-2 is-flex" type="is-success" v-for="(category,i) in categories" :key="category.i">
 
-                <b-icon :icon="category.icon" style="margin-left:7px;margin-right:5px"> </b-icon>
+                  <b-icon :icon="category.icon" class="mx-2"> </b-icon>
 
-                <span class="has-text-weight-medium" style="white-space:pre">{{category.name}}</span>
+                  <span class="has-text-weight-medium" style="white-space:pre">{{category.name}}</span>
 
-              </b-radio>
+                </b-radio>
 
-            </div>
+              </div>
 
-        </div>
+          </div>
 
-    </div>
-
-    <div v-if="!$fetchState.pending" class="main-content">
-
-      <nav class="level" style="margin-top:15px">
-
-        <div class="level-left">
-          <p class="has-text-weight-medium">{{count}} товарів</p>
-        </div>
-
-        <div class="level-right">
-          <p @click="$store.commit('updateSort', 'price_asc')" :class="{'has-text-success': sort == 'price_asc' || sort == 'price-asc'}" style="cursor:pointer" class="level-item has-text-weight-medium">Дешевші</p>
-          <p @click="$store.commit('updateSort', 'price_desc')" :class="{'has-text-success': sort == 'price_desc' || sort == 'price-desc'}" style="cursor:pointer" class="level-item has-text-weight-medium">Дорожчі</p>
-        </div>
-
-      </nav>
-
-      <div class="columns is-multiline" style="margin-top:-10px">
-        <div class="column is-3" v-for="(good,index) in goods" :key="good.ean">
-          <Card :good="good" :store="storeSelect" />
-        </div>
       </div>
 
-      <Pagination :count="count" :page="page" />
+      <div class="ml-6 mr-5" v-if="!$fetchState.pending">
+
+        <nav class="level mt-6">
+
+          <div class="level-left">
+            <p class="level-item has-text-weight-medium">{{count}} товарів</p>
+          </div>
+
+          <div class="level-right">
+            <p @click="$store.commit('updateSort', 'price_asc')" :class="{'has-text-success': sort == 'price_asc' || sort == 'price-asc'}" class="level-item is-clickable has-text-weight-medium">Дешевші</p>
+            <p @click="$store.commit('updateSort', 'price_desc')" :class="{'has-text-success': sort == 'price_desc' || sort == 'price-desc'}"  class="level-item is-clickable has-text-weight-medium">Дорожчі</p>
+          </div>
+
+        </nav>
+
+        <div class="columns is-multiline">
+          <div class="column is-3" v-for="(good,i) in goods" :key="good.i">
+            <Card :good="good" :store="storeSelect" />
+          </div>
+        </div>
+        <Pagination :count="count" :page="page" />
+      </div>
 
     </div>
 
-    </div>
-
-</div>
-
+  </div>
 </template>
 
 <script>
@@ -206,8 +202,6 @@ export default {
 .dropdown-content{font-size:.875rem;padding-bottom:.5rem;padding-top:.5rem;border:1px solid #dbdbdb;border-radius:6px}
 .dropdown-menu{min-width:110px!important;transition-duration:86ms;transition-property:opacity,transform}
 .dropdown-item{padding:.375rem 1rem;padding-right:3rem;white-space:nowrap}
-.sidebar{background-color:#F2F2F2;border-width:0 1px 0 0;position:fixed;top:50px;left:0;bottom:0;width:250px;overflow:auto}
-.main-content{max-width:100%;padding:16px 32px;margin:0 auto;display:block}
 span.icon.is-left{margin:16px}span.control-label{display:contents}
 .card{height:100%}
 </style>
