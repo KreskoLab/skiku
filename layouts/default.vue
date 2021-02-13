29ca1f47c076e82ab5c1716883da4be01e063311
KreskoLab <redeezol@gmail.com>
2021-02-13 23:01:58 +0200

    add transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,16 @@
 <template>
   <div>
-    
     <nuxt />
-    
   </div>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-     
-    }
-  }
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
 }
-</script>
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
