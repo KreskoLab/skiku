a47465a2e26a5bc23a6fa67e583806db84bf2582
KreskoLab <redeezol@gmail.com>
2021-02-14 15:29:05 +0200

    mobile modal

--- plugins/buefy.js
+++ plugins/buefy.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
-import { Input, Navbar, Button, Dropdown, Radio, Tag, Skeleton, Pagination, Icon } from 'buefy'
+import { Navbar, Button, Dropdown, Radio, Tag, Pagination, Modal } from 'buefy'
 
-Vue.use(Input)
 Vue.use(Navbar)
 Vue.use(Button)
 Vue.use(Dropdown)
 Vue.use(Radio)
 Vue.use(Tag)
-Vue.use(Skeleton)
 Vue.use(Pagination)
-Vue.use(Icon)
+Vue.use(Modal)
